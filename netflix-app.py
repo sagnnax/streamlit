1e3ec38f1e5bc355806b950a8a0c39cdca903265
mxaxel <axel.gustavo2001@outlook.com>
2023-03-02 13:08:17 -0600

    add code

--- netflix-app.py
+++ netflix-app.py
@@ -1,29 +1,39 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import codecs
 
+
+
+#Config. de pagina
 st.title('Netflix App')
 name_link ='https://raw.githubusercontent.com/sagnnax/streamlit/master/movies.csv'
 
-@st.cache
-def load_data(nrows):
-    name_link = codecs.open('movies.csv', mode='r', encoding=None, errors='strict', buffering=- 1)
+st.text('Axel Gustavo Peña Sánchez - S20006742')
 
+
+@st.cache
+
+def load_data(nrows):
+    name_link = codecs.open("movies.csv",'r','latin1')
     data = pd.read_csv(name_link, nrows=nrows)
     return data
 
-data_load_state = st.text('cargando')
-data = load_data(500)
-data_load_state.text('Axel Gustavo Peña Sánchez - S20006742')
-
-st.dataframe(data)
 
 
+
+#Contenido del sidebar
 sidebar = st.sidebar
 st.sidebar.image("https://raw.githubusercontent.com/sagnnax/streamlit/master/logo.jpg")
 sidebar.title("Filtro")
-agree = st.sidebar.checkbox("Mostrar todos los filmes")
+
+agree=sidebar.checkbox("¿Desea ver los films recuperados?")
 if agree:
-  load_data(500)
-sidebar.write("Nombre de la pelicula que desea buscar:")
-sidebar.text_input("movie: ")
+    data_load_state = st.text('cargando')
+    data = load_data(500)
+    data_load_state.text('netflix app!! (using st.cache)')
+    st.dataframe(data)
+
+sidebar.markdown("##")
+
+title=sidebar.text_input("Ingresa el titulo de un pelicula:")
+agree=sidebar.button
