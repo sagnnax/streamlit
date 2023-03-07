5a7905b05ebd87c724fd6fa0f4957b3f6aa765d1
mxaxel <axel.gustavo2001@outlook.com>
2023-03-07 10:13:18 -0600

    add code nyc

---
+++ nyc.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Cicle Rides in NYC')
+st.text('Axel Gustavo Peña Sánchez - S20006742')
+st.text('zS20006742@estudiantes.uv.mx')
+
+DATE_COLUMN = 'started_at'
+DATA_URL = ('citibike-tripdata.csv')
+
+@st.cache_resource
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename({'start_lat': 'lat', 'start_lng': 'lon'}, axis=1, inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading cicle nyc data...')
+data = load_data(1000)
+data_load_state.text("Done! (using st.cache)")
+
+
+st.sidebar.image("https://raw.githubusercontent.com/sagnnax/streamlit/master/logo.jpg")
+if st.sidebar.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+if st.sidebar.checkbox('Recorridos por hora'):
+    st.subheader('Numero de recorridos por hora')
+
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
+
+
+# Some number in the range 0-23
+hour_to_filter = st.sidebar.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Mapa de recorridos iniciado a las %s:00' % hour_to_filter)
+st.map(filtered_data)
